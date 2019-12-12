7e2f04cb46d338fc4e46872e466233aedf5fd7bd
Qoosim <ayinde.abdulghaniyy@gmail.com>
2019-12-12 13:13:58 +0100

    feat: expression added to my_each method

--- enumerables.rb
+++ enumerables.rb
@@ -2,7 +2,12 @@
 module Enumerable
 
   def my_each
-
+    k = 0
+    while k < self.size
+      yield(self[k])
+      k += 1
+    end
+    self
   end
 
   def my_each_with_index
@@ -39,6 +44,8 @@ module Enumerable
 
 end
 
+arr1 = [2, 1, 4, 5, 3, 6, 7]
 
+puts arr1.my_each{|k| k*2}
 
 # rubocop:enable all
