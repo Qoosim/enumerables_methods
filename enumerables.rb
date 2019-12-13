9f345210aea88b9342aef76ece627bb3a4989946
Qoosim <ayinde.abdulghaniyy@gmail.com>
2019-12-13 23:08:34 +0100

    feat: my_inject method added

--- enumerables.rb
+++ enumerables.rb
@@ -1,6 +1,7 @@
 # rubocop:disable all
 
 module Enumerable
+
   # my_each method that prints the square of every elements in an array 
   def my_each
     k = 0
@@ -10,6 +11,7 @@ module Enumerable
     end
     self
   end
+
   # my_each_with_index that prints double of each element of a particular position
   def my_each_with_index
     k = 0
@@ -18,6 +20,7 @@ module Enumerable
       k += 1
     end
   end
+
   # my_select method that outputs even numbers of the given array
   def my_select
     k = 0
@@ -45,23 +48,36 @@ module Enumerable
 
   end
 
+  # my_map method that output the trasformed of a given array
   def my_map
-
+    k = 0
+    trans_array = []
+    while k < size
+      trans_array << yield(self[k])
+      k += 1
+    end
+    trans_array
   end
 
-  def my_inject
-    
+  def my_inject(inital_value)
+    my_each{ |element| inital_value = yield(inital_value, element)}
+    inital_value
   end
-  
+
 end
-array_test1 = [2, 5, 6, 3, 8]
-array_test2 = [3, 8, 7, 9]
-array_test3 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 
-array_test1.my_each{|k| print "#{k*2} "}
+array_each = [2, 5, 6, 3, 8]
+array_index = [3, 8, 7, 9]
+array_select = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+array_inject = [4, 6, 9, 6, 2]
+animals = ["cat", "dog", "cow", "ram", "hen"]
+
+array_each.my_each{|k| print "#{k**2} "}
 puts
-array_test2.my_each_with_index{|k| print "#{k} "}
+array_index.my_each_with_index{|k| print "#{k} "}
 puts
-print array_test3.my_select{|even| even}
-puts
+p array_select.my_select{|even| even}
+p animals.my_map{|animal| animal.capitalize}
+p array_inject.my_inject(0){|sum, num| sum ++ num}
+
 # rubocop:enable all
