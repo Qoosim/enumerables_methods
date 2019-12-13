c35d9abb2c96c5892c5be8aefa273bad40c2f9a7
Qoosim <ayinde.abdulghaniyy@gmail.com>
2019-12-13 18:33:38 +0100

    feat: each_with_index and my_select methods added

--- enumerables.rb
+++ enumerables.rb
@@ -1,21 +1,32 @@
 # rubocop:disable all
-module Enumerable
 
+module Enumerable
+  # my_each method that prints the square of every elements in an array 
   def my_each
     k = 0
-    while k < self.size
-      yield(self[k])
+    while k < size
+      yield self[k]
       k += 1
     end
     self
   end
-
+  # my_each_with_index that prints double of each element of a particular position
   def my_each_with_index
-
+    k = 0
+    while k < size
+      yield(self[k], k)
+      k += 1
+    end
   end
-
+  # my_select method that outputs even numbers of the given array
   def my_select
-
+    k = 0
+    array_result = []
+    while k < size
+      array_result << self[k] if yield(self[k] % 2 == 0)
+      k += 1
+    end
+    array_result
   end
 
   def my_all?
@@ -41,11 +52,16 @@ module Enumerable
   def my_inject
     
   end
-
+  
 end
+array_test1 = [2, 5, 6, 3, 8]
+array_test2 = [3, 8, 7, 9]
+array_test3 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 
-arr1 = [2, 1, 4, 5, 3, 6, 7]
-
-puts arr1.my_each{|k| k*2}
-
+array_test1.my_each{|k| print "#{k*2} "}
+puts
+array_test2.my_each_with_index{|k| print "#{k} "}
+puts
+print array_test3.my_select{|even| even}
+puts
 # rubocop:enable all
