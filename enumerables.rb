bfec02cb5362d84222a900b551bfa4467a5a6cf2
Qoosim <ayinde.abdulghaniyy@gmail.com>
2019-12-17 00:01:08 +0100

    required changes corrected

--- enumerables.rb
+++ enumerables.rb
@@ -28,14 +28,11 @@ module Enumerable
   # my_select method that outputs even numbers of the given array
   def my_select
     return to_enum(:my_select) unless block_given?
+  
+    new_arr = []
+    my_each { |element| new_arr << element if yield(element) }
+    new_arr
 
-    k = 0
-    array_result = []
-    while k < size
-      array_result << self[k] if yield(self[k] % 2 == 0)
-      k += 1
-    end
-    array_result
   end
 
   def my_all?(*arg)
@@ -58,27 +55,22 @@ module Enumerable
     false
   end
 
-  def my_none?
-    return to_enum(:my_none?) unless block_given?
+  def my_none?(*arg)
+    return grep(arg.first).empty? unless arg.empty?
 
-    k = 0
-    while k < size
-      return false if yield(self[k])
-      k += 1
-    end
+    my_each { |el| return false if yield(el) } if block_given?
+
+    my_each { |el| return false if el } unless block_given?
+
     true
   end
 
-  def my_count
-    return to_enum(:my_count) unless block_given?
+  def my_count(*arg)
+    return my_select { |el| el == arg.first }.length unless arg.empty?
 
-    k = 0
-    kounter = 0
-    while k < size
-      kounter += 1 if yield(self[k])
-      k += 1
-    end
-    kounter
+    return my_select { |el| yield el }.length if block_given?
+
+    size
   end
 
   # my_map method that output the trasformed of a given array
@@ -95,14 +87,24 @@ module Enumerable
   end
 
   # my_inject method that prints the sum of all the elements of a given array
-  def my_inject(initial_value)
-    return to_enum(:my_inject) unless block_given?
-
-    my_each{ |element| initial_value = yield(initial_value, element)}
-    initial_value
-
+  def my_inject(*arg)
+    res = nil
+    if arg.first && [Integer, Float].include?(arg.first.class)
+      res = arg.first
+      arr = to_a
+    else
+      res = first
+      arr = to_a[1..-1]
+    end
+    if block_given?
+      arr.my_each { |el| res = yield(res, el) }
+    elsif arg.last.is_a? Symbol
+      arr.my_each { |el| res = arg.last.to_proc.call(res, el) }
+    else
+      res = to_enum
+    end
+    res
   end
-
 end
 
 def multiply_els(arr)
