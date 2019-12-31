9a8af089e1fe3f56403e153ed11af81ece3003ea
Qoosim <ayinde.abdulghaniyy@gmail.com>
2019-12-31 16:40:25 +0100

    global array declared

--- spec/enumerable_spec.rb
+++ spec/enumerable_spec.rb
@@ -2,9 +2,10 @@
 
 require './lib/enumerables'
 RSpec.describe Enumerable do
+  let(:arr) = []
   describe '#my_each' do
     it 'returns elements of array if block is given, otherwise enumerator is given ' do
-      any = 0
+      arr = 0
       [1, 2, 3, 4, 5].my_each { any += 1 }
       expect(any).to eql(5)
     end
